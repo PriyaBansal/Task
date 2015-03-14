2b656bf84af0eb21721061d119eec081b6d46008
PriyaBansal <priyabansal.hr@gmail.com>
2015-03-14 19:03:27 +0530

    First commit

---
+++ foo.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<string.h>
+
+int foo(const char* str){
+	printf("%s\n", str);
+	return 0;
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"foo.h"
+
+int main(){
+	printf("%d\n",foo("Hello World\n"));
+	return 0;
+}
